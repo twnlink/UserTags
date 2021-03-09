eab80f3fe072680c9d2ad6d801b3d2ef37d7bd1d
creatable <31049082+Cr3atable@users.noreply.github.com>
2021-03-09 17:25:41 -0500

    Update goosemod.js

--- goosemod.js
+++ goosemod.js
@@ -1,1 +1,15 @@
+let style;
 
+export default {
+  goosemodHandlers: {
+    onImport: async () => {
+      style = document.createElement("style");
+      style.textContent = `@import "https://raw.githack.com/TheRealGWJosh/CustomUserTags/main/main/main.css"`;
+      document.head.appendChild(style);
+    },
+
+    onRemove: async () => {
+      style.remove();
+    },
+  },
+};
