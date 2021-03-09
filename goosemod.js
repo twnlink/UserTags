28861989b9785875f270c20cc8b1f40b716690f0
creatable <31049082+Cr3atable@users.noreply.github.com>
2021-03-09 17:25:56 -0500

    lol wrong import

--- goosemod.js
+++ goosemod.js
@@ -4,7 +4,7 @@ export default {
   goosemodHandlers: {
     onImport: async () => {
       style = document.createElement("style");
-      style.textContent = `@import "https://raw.githack.com/TheRealGWJosh/CustomUserTags/main/main/main.css"`;
+      style.textContent = `@import "https://raw.githack.com/Cr3atable/UserTags/master/basic.css"`;
       document.head.appendChild(style);
     },
 
